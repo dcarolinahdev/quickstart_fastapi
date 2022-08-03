59defd53f012ee30babb96a7656aee866a5cad83
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-08-03 11:34:55 -0500

    Validations min_length, max_length

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 # Pydantic
 from pydantic import BaseModel
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -24,3 +24,13 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     pass
+
+# Validations: Query Parameters
+
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    # age: str = Query(...)
+    age: Optional[int] = Query(0)
+):
+    return {name: age}
